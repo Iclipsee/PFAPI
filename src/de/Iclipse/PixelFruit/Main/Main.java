3aab29a98df8b9ce8793bb568da709fe3ed2d1b9
Iclipsee <iclipskii@gmail.com>
2019-04-20 11:11:12 +0200

    First working API

---
+++ src/de/Iclipse/PixelFruit/Main/Main.java
@@ -0,0 +1,20 @@
+package de.Iclipse.PixelFruit.Main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Yannick who could get really angry if somebody steal his code!
+ * ~Yannick on 18.04.2019 at 10:36 o´ clock
+ */
+public class Main  extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
